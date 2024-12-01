7a5426cf6e95b846d41587bbe2a577ea8b5a41da
derkalaender <git@derkalaender.de>
2024-12-01 11:11:13 +0100

    Day01 template

---
+++ days/day01/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "aoc2024"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
+
+kotlin {
+    jvmToolchain(23)
+}
